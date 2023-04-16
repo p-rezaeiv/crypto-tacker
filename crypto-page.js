00da46509f93bc6c8a4062aa6797caaf1610c800
Amir Yazdani <a_yazdani@isc.co.ir>
2023-04-16 12:00:17 +0330

    somehow dynamic cryptopage

--- crypto-page.js
+++ crypto-page.js
@@ -1,28 +1,32 @@
 const params = new Proxy(new URLSearchParams(window.location.search), {
   get: (searchParams, prop) => searchParams.get(prop),
 });
+
+// let cryptoInfo = {
+//   "id": "1e31218a-e44e-4285-820c-8282ee222035",
+//   "name": "BITCOIN",
+//   "slug": "BITCOIN",
+//   "symbol": "btc",
+//   "metrics": {
+//     "market_data": {
+//       "price_usd": 30450.68673965129700,
+//       "percent_change_usd_last_24_hours": -0.9732848144396884
+//     }
+//   }
+// }
+let cryptoInfo = {};
 // Get the value of "some_key" in eg "https://example.com/?some_key=some_value"
 let symbol = params.symbol; // "some_value"
 
+getDataBySymbol(symbol, (data) => {
+  cryptoInfo = data;
+  createCryptoNameElement();
+  createCryptoSymbolElement();
+  createCryptopriceusdElement();
+  createCoinPercentChangeElement();
+  createCryptoCoinImgElement();
+})
 
-const cryptoInfo = {
-  "id": "1e31218a-e44e-4285-820c-8282ee222035",
-  "name": "BITCOIN",
-  "slug": "bitcoin",
-  "symbol": symbol,
-  "metrics": {
-    "market_data": {
-      "price_usd": 30450.68673965129700,
-      "percent_change_usd_last_24_hours": -0.9732848144396884
-    }
-  }
-}
-
-createCryptoNameElement();
-createCryptoSymbolElement();
-createCryptopriceusdElement();
-createCoinPercentChangeElement();
-createCryptoCoinImgElement();
 
 
 function createCryptoNameElement() {
@@ -38,12 +42,12 @@ function createCryptoSymbolElement() {
 }
 function createCryptopriceusdElement() {
   const cryptopriceusd = document.getElementById('coinPrice')
-  cryptopriceusd.textContent = cryptoInfo.metrics.market_data.price_usd.toFixed(2)+' '+'USD';
+  cryptopriceusd.textContent = cryptoInfo.metrics.market_data.price_usd.toFixed(2) + ' ' + 'USD';
 }
 
 function createCoinPercentChangeElement() {
   const CoinPercentChange = document.getElementById('coinPercentChange')
-  CoinPercentChange.textContent = '('+cryptoInfo.metrics.market_data.percent_change_usd_last_24_hours.toFixed(2)+'%)';
+  CoinPercentChange.textContent = '(' + cryptoInfo.metrics.market_data.percent_change_usd_last_24_hours.toFixed(2) + '%)';
 }
 
 function createCryptoCoinImgElement() {
