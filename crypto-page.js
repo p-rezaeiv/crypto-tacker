94cb7304997ad917e94443fea8b612e1c25a76a5
Amir Yazdani <a_yazdani@isc.co.ir>
2023-04-16 11:14:07 +0330

    create link to mainpage

--- crypto-page.js
+++ crypto-page.js
@@ -1,8 +1,15 @@
+const params = new Proxy(new URLSearchParams(window.location.search), {
+  get: (searchParams, prop) => searchParams.get(prop),
+});
+// Get the value of "some_key" in eg "https://example.com/?some_key=some_value"
+let symbol = params.symbol; // "some_value"
+
+
 const cryptoInfo = {
   "id": "1e31218a-e44e-4285-820c-8282ee222035",
   "name": "BITCOIN",
   "slug": "bitcoin",
-  "symbol": "BTC",
+  "symbol": symbol,
   "metrics": {
     "market_data": {
       "price_usd": 30450.68673965129700,
@@ -16,6 +23,7 @@ createCryptoSymbolElement();
 createCryptopriceusdElement();
 createCoinPercentChangeElement();
 createCryptoCoinImgElement();
+
 
 function createCryptoNameElement() {
   const cryptoName = document.getElementById('cryptoName')
