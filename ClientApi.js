24150d60d75374dacd41c58550a710845aee675e
Parvin Rezaei <parvinrezaei2014@gmail.com>
2023-04-16 12:02:18 +0330

    search complete

--- ClientApi.js
+++ ClientApi.js
@@ -3,7 +3,7 @@ function getData(callback) {
     $.ajax('https://data.messari.io/api/v1/assets?fields=id,name,slug,symbol,metrics/market_data/price_usd,metrics/market_data/percent_change_usd_last_24_hours,metrics/marketcap/current_marketcap_usd', 
     {
         dataType: 'json', // type of response data
-        timeout: 500,     // timeout milliseconds
+        timeout: 2000,     // timeout milliseconds
         success: function (data,status,xhr) {   // success callback function
             callback(data)
         
